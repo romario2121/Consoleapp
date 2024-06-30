6833e94c2f6cb0ed42449c86acd8d4fcb177c1f6
romario2121 <80700951+romario2121@users.noreply.github.com>
2024-06-30 23:05:33 +0300

    Update ConsoleApplication10.cpp

--- ConsoleApplication10/ConsoleApplication10.cpp
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -1,252 +1,71 @@
-﻿#include <iostream>
-#include <string>
+#include <iostream>
 using namespace std;
-
-
-class Vehicle
-{
-public:
-
-    virtual string getType() const = 0;
-    virtual int getNumPlace() const = 0;
-    virtual ~Vehicle() {}
-};
-
-
-class Car : public Vehicle 
-
-{
-private:
-    int numPlace;
-public:
-
-   
-    Car(int seats) : numPlace(seats) {}
-
-
-    string getType() const override 
-    {
-        return "Car";
-    }
-
-
-    int getNumPlace() const override
-    {
-        return numPlace;
-    }
-};
-
-
-class Bus : public Vehicle 
-{
-private:
-    int numPlace;
-public:
- 
-    Bus(int seats ) : numPlace(seats) {}
-
- 
-    string getType() const override
-    {
-        return "Bus";
-    }
-
-   
-    int getNumPlace() const override
-    {
-        return numPlace;
-    }
-};
-
 
 int main()
+
 {
-  
-    Car car(7);  
-    Bus bus(25); 
+    int a, b;
 
-    cout << "Type  car: " << car.getType() << endl;
-    cout << "Number  seats in car: " << car.getNumPlace() << endl;
+    cout << "Enter two integers: ";
+    cin >> a >> b;
 
-    cout << "Type bus: " << bus.getType() << endl;
-    cout << "Number seats in bus: " << bus.getNumPlace() << endl;
+    cout << "Sum23: " << a * b << endl;
+
+    cerr << "This is an error message." << endl;
+
+    clog << "Log information." << endl;
 
     return 0;
 }
-///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+ {
+    string fullName;
+
+    cout << "Enter your external name: ";
+
+    getline(cin, fullName);
+
+    cout << "Your outside name: " << fullName << std::endl;
+
+    return 0;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////
+
 #include <iostream>
 #include <string>
 using namespace std;
 
-class Vehicle 
-{
-public:
-    virtual string getType() const = 0;
-    virtual int getNumPlace() const = 0;
-    virtual ~Vehicle() {}
-};
+int main()
+ {
+    string phrase;
+    cout << "Enter a phrase: ";
+    getline(std::cin, phrase);
+
+    string result;
 
 
-class Car : public Vehicle
-
-{
-protected:
-    int numPlace;  
-public:
-    Car(int seats) : numPlace(seats) {}
-
-  
-    string getType() const override
+    for (char ch : phrase)
     {
-        return "Car";
+        if (ch == '!')
+        {
+            result += '$';
+        } else if (ch != '#')
+         {
+            result += ch;
+         }
+
     }
 
-
-    int getNumPlace() const override
-    {
-        return numPlace;
-    }
-
-    
-    virtual string getAdditionalInfo() const
-    {
-        return "Universal Car";
-    }
-};
-
-
-class SportsCar : public Car
-{
-public:
-    SportsCar(int seats) : Car(seats) {}
-
-    string getType() const override 
-    {
-        return "Sports Car";
-    }
-
-    
-    int getNumPlace() const override 
-    {
-        return Car::getNumPlace();
-    }
-
-    string getAdditionalInfo() const override 
-    {
-        return "Sports Car specific ";
-    }
-};
-
-
-class Wagon : public Car
-{
-public:
-    Wagon(int seats) : Car(seats) {}
-
-    string getType() const override 
-    {
-        return "Wagon";
-    }
-
-    int getNumPlace() const override 
-    {
-        return Car::getNumPlace();
-    }
-
-    string getAdditionalInfo() const override 
-    {
-        return "Wagon specific ";
-    }
-};
-
-
-class Coupe : public Car 
-{
-public:
-    Coupe(int seats) : Car(seats) {}
-
-
-    string getType() const override
-    {
-        return "Coupe";
-    }
-
-    int getNumPlace() const override 
-    {
-        return Car::getNumPlace();
-    }
-
-    string getAdditionalInfo() const override 
-    {
-        return "Coupe specific ";
-    }
-};
-
-int main() 
-{
-   
-    SportsCar sportsCar(2);
-    Wagon wagon(8);
-    Coupe coupe(2);
-
-    cout << "Type of sports car: " << sportsCar.getType() << endl;
-    cout << "Number of seats in sports car: " << sportsCar.getNumPlace() << endl;
-    cout << "Additional info: " << sportsCar.getAdditionalInfo() << endl;
-
-    cout << "Type of wagon: " << wagon.getType() << endl;
-    cout << "Number of seats in wagon: " << wagon.getNumPlace() << endl;
-    cout << "Additional info: " << wagon.getAdditionalInfo() << endl;
-
-    cout << "Type of coupe: " << coupe.getType() << endl;
-    cout << "Number of seats in coupe: " << coupe.getNumPlace() << endl;
-    cout << "Additional info: " << coupe.getAdditionalInfo() << endl;
-
-    return 0;
-}
-///////////////////////////////////////////////////////
-#include <iostream>
-using namespace std;
-
-class AClass 
-{
-private:
-    int Variable;  
-    static int staticVariable;  
-
-public:
-    
-    AClass(int value) : Variable(value)
-    {
-        staticVariable++;  
-    }
-    ~AClass() 
-    {
-        staticVariable--;  
-    }
-
-    static int getStaticVariable() 
-    {
-        return staticVariable;
-    }
-
-    int getVariable() 
-    
-    {
-        return Variable;
-    }
-};
-
-int AClass::staticVariable = 0;
-
-int main() 
-{
-
-    AClass A1(23);
-    AClass B2(33);
-
-    cout << "A1 Variable: " << A1.getVariable() << endl;
-    cout << "B2 Variable: " << B2.getVariable() << endl;
-    cout << "Static variable: " << AClass::getStaticVariable() << endl;
+    cout << "Modified phrase: " << result << endl;
 
     return 0;
 }
