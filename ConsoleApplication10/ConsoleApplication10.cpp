bcddf4bb8369f23c2b4a7134f3ce9af118fb178d
romario2121 <80700951+romario2121@users.noreply.github.com>
2024-07-02 18:28:06 +0300

    Update ConsoleApplication10.cpp

--- ConsoleApplication10/ConsoleApplication10.cpp
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -1,71 +1,355 @@
 #include <iostream>
 using namespace std;
 
-int main()
+class MyClass
+
 
 {
-    int a, b;
+private:
+    int regulVariable;
+    static int staticVariable;
 
-    cout << "Enter two integers: ";
-    cin >> a >> b;
+public:
 
-    cout << "Sum23: " << a * b << endl;
-
-    cerr << "This is an error message." << endl;
-
-    clog << "Log information." << endl;
-
-    return 0;
-}
-//////////////////////////////////////////////////////////////////////////////////
-
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-int main()
- {
-    string fullName;
-
-    cout << "Enter your external name: ";
-
-    getline(cin, fullName);
-
-    cout << "Your outside name: " << fullName << std::endl;
-
-    return 0;
-}
-
-
-////////////////////////////////////////////////////////////////////////////////////
-
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main()
- {
-    string phrase;
-    cout << "Enter a phrase: ";
-    getline(std::cin, phrase);
-
-    string result;
-
-
-    for (char ch : phrase)
+    MyClass (int value) : regulVariable(value)
     {
-        if (ch == '!')
-        {
-            result += '$';
-        } else if (ch != '#')
-         {
-            result += ch;
-         }
-
+        staticVariable++;
     }
 
-    cout << "Modified phrase: " << result << endl;
+
+    ~MyClass()
+    {
+        staticVariable--;
+    }
+
+
+    int getRegulVariable()
+    {
+        return regulVariable;
+    }
+
+
+    static int getStaticVariable()
+    {
+        return staticVariable;
+    }
+};
+
+
+int MyClass::staticVariable = 0;
+
+int main()
+{
+
+    MyClass object1(3);
+    MyClass object2(5);
+
+    cout << "object1.Variable = " << object1.getRegulVariable() << endl;
+    cout << "object2.Variable = " << object2.getRegulVariable() << endl;
+    cout << "MyClass-staticVariable = " << MyClass::getStaticVariable() << endl;
+
+
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+using namespace std;
+
+class MyClass
+
+{
+
+private:
+
+    int regulVariable;
+    static int staticVariable;
+
+public:
+
+    MyClass(int value) : regulVariable(value)
+    {
+        staticVariable++;
+    }
+
+    ~MyClass()
+    {
+        staticVariable--;
+    }
+
+    int getRegulVariable()
+    {
+        return regulVariable;
+    }
+
+    static int getStaticVariable()
+    {
+        return staticVariable;
+    }
+};
+
+
+int MyClass::staticVariable = 0;
+
+int main()
+{
+
+    MyClass object1(3);
+    MyClass object2(5);
+    MyClass object3(7);
+
+    cout << "object1.Variable = " << object1.getRegulVariable() << endl;
+    cout << "object2.Variable = " << object2.getRegulVariable() << endl;
+    cout << "object3.Variable = " << object3.getRegulVariable() << endl;
+    cout << "MyClass-staticVariable = " << MyClass::getStaticVariable() << endl;
+
+
+    cout << "Deleting object1..." << endl;
+    cout << "MyClass-staticVariable = " << MyClass::getStaticVariable() << endl;
+    cout << "Deleting object2..." << endl;
+    cout << "MyClass-staticVariable = " << MyClass::getStaticVariable() << endl;
+    cout << "Deleting object3..." << endl;
+    cout << "MyClass-staticVariable = " << MyClass::getStaticVariable() << endl;
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+using namespace std;
+
+class MyClass
+{
+private:
+    int ordinaryMember;
+    static int staticMember;
+
+public:
+
+    MyClass(int value) : ordinaryMember(value)
+    {
+        incrementStaticMember();
+    }
+
+    ~MyClass()
+    {
+        decrementStaticMember();
+    }
+
+    int getOrdinaryMember() const
+    {
+        return ordinaryMember;
+    }
+
+    static int getStaticMember()
+    {
+        return staticMember;
+    }
+
+private:
+
+    static void incrementStaticMember()
+    {
+        staticMember++;
+    }
+
+    static void decrementStaticMember()
+    {
+        staticMember--;
+    }
+};
+
+/// /////////////////////////////////////////////////////////////////////
+
+int MyClass::staticMember = 0;
+
+int main()
+{
+
+    MyClass obj1(3);
+    MyClass obj2(5);
+    MyClass obj3(7);
+
+    cout << "obj1 ordinaryMember: " << obj1.getOrdinaryMember() << endl;
+    cout << "obj2 ordinaryMember: " << obj2.getOrdinaryMember() << endl;
+    cout << "obj3 ordinaryMember: " << obj3.getOrdinaryMember() << endl;
+    cout << "Static member after creation: " << MyClass::getStaticMember() << endl;
+
+    cout << "Deleting obj3..." << endl;
+    cout << "Static member after deleting obj3: " << MyClass::getStaticMember() << endl;
+    cout << "Deleting obj2..." << endl;
+    cout << "Static member after deleting obj2: " << MyClass::getStaticMember() << endl;
+    cout << "Deleting obj1..." << endl;
+    cout << "Static member after deleting obj1: " << MyClass::getStaticMember() << endl;
+
+    return 0;
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+using namespace std;
+class MyClass
+{
+private:
+    int ordinaryMember;
+    static int staticMember;
+
+public:
+
+    MyClass(int value) : ordinaryMember(value)
+    {
+        incrementStaticMember();
+    }
+
+    ~MyClass()
+    {
+        decrementStaticMember();
+    }
+
+    int getOrdinaryMember() const
+    {
+        return ordinaryMember;
+    }
+
+    static int getStaticMember()
+    {
+        return staticMember;
+    }
+
+    int (MyClass::* getOrdinaryMemberPointer)() const = &MyClass::getOrdinaryMember;
+
+private:
+    static void incrementStaticMember()
+    {
+        staticMember++;
+    }
+
+    static void decrementStaticMember()
+    {
+        staticMember--;
+    }
+};
+
+int MyClass::staticMember = 0;
+
+int main()
+{
+
+    MyClass obj1(3);
+    MyClass obj2(5);
+    MyClass obj3(7);
+
+
+    cout << "Using member function pointer:" << std::endl;
+    cout << "obj1 ordinaryMember: " << (obj1.*(obj1.getOrdinaryMemberPointer))() << endl;
+    cout << "obj2 ordinaryMember: " << (obj2.*(obj2.getOrdinaryMemberPointer))() << endl;
+    cout << "obj3 ordinaryMember: " << (obj3.*(obj3.getOrdinaryMemberPointer))() << endl;
+
+
+    cout << "Static member after creation: " << MyClass::getStaticMember() << std::endl;
+
+
+   cout << "Deleting obj3..." << std::endl;
+
+    cout << "Static member after deleting obj3: " << MyClass::getStaticMember() << endl;
+    cout << "Deleting obj2..." << std::endl;
+    cout << "Static member after deleting obj2: " << MyClass::getStaticMember() << endl;
+    cout << "Deleting obj1..." << std::endl;
+    cout << "Static member after deleting obj1: " << MyClass::getStaticMember() << endl;
+
+    return 0;
+}
+
+
+#include <iostream>
+using namespace std;
+class MyClass
+{
+private:
+    int regularVariable;       
+    static int staticVariable;  
+    double doubleVariable;       
+    std::string stringVariable;  
+
+public:
+   
+    MyClass(int intValue, double doubleValue, const std::string& stringValue)
+        : regularVariable(intValue), doubleVariable(doubleValue), stringVariable(stringValue) 
+    {
+        staticVariable++; 
+    }
+
+    ~MyClass() {
+        staticVariable--; 
+    }
+
+
+    int getRegularVariable() const
+    {
+        return regularVariable;
+    }
+
+   
+    static int getStaticVariable() 
+    {
+        return staticVariable;
+    }
+
+  
+    double getDoubleVariable() const 
+    {
+        return doubleVariable;
+    }
+
+
+    std::string getStringVariable() const 
+    {
+        return stringVariable;
+    }
+
+   
+    using MemberFunctionPtr = decltype(&MyClass::getRegularVariable);
+    MemberFunctionPtr ptrGetVariable = &MyClass::getRegularVariable;
+};
+
+
+int MyClass::staticVariable = 0;
+
+int main()
+{
+
+    MyClass obj1(3, 3.1, "Roman");
+    MyClass obj2(5, 5.1, "Davydko");
+    MyClass obj3(7, 7.1, ".");
+
+  
+    cout << "obj1: Regular Variable = " << (obj1.*obj1.ptrGetVariable)()
+        << ", Double Variable = " << obj1.getDoubleVariable()
+        << ", String Variable = " << obj1.getStringVariable()
+        << ", Static Variable = " << MyClass::getStaticVariable() << endl;
+
+    cout << "obj2: Regular Variable = " << (obj2.*obj2.ptrGetVariable)()
+        << ", Double Variable = " << obj2.getDoubleVariable()
+        << ", String Variable = " << obj2.getStringVariable()
+        << ", Static Variable = " << MyClass::getStaticVariable() << std::endl;
+
+    cout << "obj3: Regular Variable = " << (obj3.*obj3.ptrGetVariable)()
+        << ", Double Variable = " << obj3.getDoubleVariable()
+        << ", String Variable = " << obj3.getStringVariable()
+        << ", Static Variable = " << MyClass::getStaticVariable() << endl;
+
+
+    cout << "Deleting obj3..." << endl;
+    MyClass::getStaticVariable();
+    cout << "Static Variable = " << MyClass::getStaticVariable() << endl;
+
+    cout << "Deleting obj2..." << endl;
+    MyClass::getStaticVariable();
+    cout << "Static Variable = " << MyClass::getStaticVariable() << endl;
+
+   cout << "Deleting obj1..." <<endl;
+    MyClass::getStaticVariable();
+    cout << "Static Variable = " << MyClass::getStaticVariable() << endl;
 
     return 0;
 }
